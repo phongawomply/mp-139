96fb0727462c950000e6c3db3c77ee2f864eb79c
Jacob Tran <jacobtran@Jacobs-MacBook-Pro.local>
2015-08-07 15:08:20 -0700

    Added top bar page object and test

---
+++ test/protractor/page-objects/top-bar.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+
+  this.getGcLogo = function() {
+    return element(by.css('.gc-logo'))
+  };
+
+  this.getGcAppLauncher = function() {
+    return element(by.css('#gc-app-launcher-toggle'))
+  };
+
+  this.getGcLocationList = function() {
+    return element(by.css('#gc-location-menu-list'))
+  };
+
+  this.getGcSelectedLocation = function() {
+    return element(by.css('#gc-location-menu-list li a'))
+  };
+};
