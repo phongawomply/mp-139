b20c3b09c977e731cd37ea49395569c4df78182f
phongawomply <phong@womply.com>
2015-08-19 16:59:16 +0800

    Change base url to local.womply.com

--- test/config/protractor.config.js
+++ test/config/protractor.config.js
@@ -1,5 +1,5 @@
 exports.config = {
-  baseUrl: 'http://localhost:9999',
+  baseUrl: 'http://local.womply.com:9999',
   seleniumServerJar: '../../node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   // Comment out 2 browsers to speed up testing
