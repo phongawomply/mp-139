0ab56d0c3231bec18a8176fb13d71c39148f5123
phongawomply <phong@womply.com>
2015-10-12 16:26:58 +0800

    [PDXAPP-271] Modify api path to allow for multiple EP

--- app/common/context/services/environment.js
+++ app/common/context/services/environment.js
@@ -25,19 +25,41 @@ angular.module('womply')
 
     return {
       /**
-       * Get the API path
+       * Get the API path as a default or as a named configuration
        *
-       * @returns {string} - the API path
+       * @returns {String} - fluent api object which extends a String
        */
       getApiPath: function() {
         var override = getOverrides();
-
         var path = 'http://local.womply.com:3000';
-        if (!isDevelopmentServer()) {
-          path = override.ApiBase || $location. protocol() + '://' + $location.host() + ($location.port() ? ':' + $location.port() : '');
-        }
+        override.API = override.API || {};
 
-        return path + (override.ApiPath || '/api/0.1');
+        var apiPath = function(apiName) {
+          return function() {
+            override.API[apiName] = override.API[apiName] || {};
+
+            if (!isDevelopmentServer()) {
+              path = override.API[apiName].host || $location. protocol() + '://' + $location.host() + ($location.port() ? ':' + $location.port() : '');
+            }
+
+            return path + (override.API[apiName].path || '/api/0.1');
+          }
+        };
+
+        var by = {
+          default: apiPath('default')
+        };
+
+        _.each(override.API, function(value, key) {
+          by[key] = apiPath(key);
+        });
+
+        var str = new String(apiPath('default')());
+
+        str.by = by;
+
+        return str;
+
       },
       /**
        * Get the insights path
