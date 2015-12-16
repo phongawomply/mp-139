b7009f120c54fc59a9db568e2eedc8fe55567ea0
phongawomply <phong@womply.com>
2015-12-16 10:09:45 +0800

    Fix protractor and protagonist

--- test/config/protractor.config.js
+++ test/config/protractor.config.js
@@ -1,7 +1,9 @@
+var seleniumServerJar = require("selenium-standalone-jar");
+
 exports.config = {
   framework: 'jasmine2',
   baseUrl: 'http://local.womply.com:9999',
-  seleniumServerJar: '../../node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  seleniumServerJar: seleniumServerJar.path,
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   // Comment out 2 browsers to speed up testing
   multiCapabilities: [
