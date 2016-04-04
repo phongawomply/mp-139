fc3d997cfa9c5dda8f01a9a66d833e4bbb867e49
Ramprasad <ramprasad@womply.com>
2016-04-04 11:32:42 -0700

    [APP-1045] Add mdThemingProvider to ap template angular

--- app/app.js
+++ app/app.js
@@ -16,6 +16,57 @@ angular.module('womply')
         }
       }
     }]);
+
+    /**
+     * Womply primary palette
+     */
+    var wombatPrimaryColors = {
+      '50': 'cde9f9',
+      '100': 'addaf6',
+      '200': '6dbeef',
+      '300': '4cafec',
+      '400': '2ca1e8',
+      '500': '0c92e5',
+      '600': '0874b7',
+      '700': '07507a',
+      '800': '053751',
+      '900': '042f44',
+      'A100': '6dbeef',
+      'A200': '4cafec',
+      'A400': '0874b7',
+      'A700': '053751',
+      'contrastDefaultColor': 'light'
+    };
+
+    /**
+     * Womply accent palette
+     */
+    var wombatAccentColors = {
+      '50': 'bff7c4',
+      '100': 'a4ebac',
+      '200': '89de95',
+      '300': '6fd27d',
+      '400': '39b9e4',
+      '500': '1ead36',
+      '600': '198d2c',
+      '700': '136c21',
+      '800': '115c1c',
+      '900': '0e4c17',
+      'A100': '89de95',
+      'A200': '6fd27d',
+      'A400': '198d2c',
+      'A700': '053751',
+      'contrastDefaultColor': 'light'
+    };
+
+    $mdThemingProvider.definePalette('wombatPalette', wombatPrimaryColors);
+    $mdThemingProvider.definePalette('wombatAccentPalette', wombatAccentColors);
+    $mdThemingProvider.theme('default')
+      .primaryPalette('wombatPalette')
+      .accentPalette('wombatAccentPalette', {
+        'default': '500',
+        'hue-1': '300'
+      });
   })
   /**
    * Main application controller which initialize the configuration and sets the
